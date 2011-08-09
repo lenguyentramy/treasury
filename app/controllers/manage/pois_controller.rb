409c3c9e989d002f1ec39d7c087174582d83e909
Tra My Le Nguyen <lenguyen@ubilabs.net>
2011-08-09 14:21:56 +0200

    added paginate

--- app/controllers/manage/pois_controller.rb
+++ app/controllers/manage/pois_controller.rb
@@ -1,3 +1,7 @@
 class Manage::PoisController < Manage::BaseController
   load_and_authorize_resource
+  
+  def index
+    @pois = Poi.paginate(:page => params[:page])
+  end
 end
--- app/controllers/manage/users_controller.rb
+++ app/controllers/manage/users_controller.rb
@@ -1,3 +1,6 @@
 class Manage::UsersController < Manage::BaseController
   load_and_authorize_resource
+  def index
+    @users = User.paginate(:page => params[:page])
+  end
 end
